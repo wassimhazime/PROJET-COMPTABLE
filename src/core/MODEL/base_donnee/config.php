42c84baf69414c2048f48c9d51f7e31db887367c
wassimhazime <wassim.hazime@gmail.com>
2017-09-25 00:02:25 +0200

    add json file config DB

--- src/core/MODEL/base_donnee/config.php
+++ src/core/MODEL/base_donnee/config.php
@@ -1,28 +1,26 @@
 <?php
+
 namespace core\model\base_donnee;
 
-class  config{
+class config {
+
     public $DB;
-    public $dbhost ;
-    public $dbuser ;
-    public $dbpass ;
-    public $dbname ;
-    
-    
-     function __construct(   $DB='mysql',
-                        $dbhost = "localhost",
-                        $dbuser = "comptable", 
-                        $dbpass = "achrafwassim", 
-                        $dbname = "awacomptable") {
-         
-                            $this->DB = $DB;
-                            $this->dbhost = $dbhost;
-                            $this->dbuser = $dbuser;
-                            $this->dbpass = $dbpass;
-                            $this->dbname = $dbname;
-        
+    public $dbhost;
+    public $dbuser;
+    public $dbpass;
+    public $dbname;
+
+    function __construct() {
+
+        $stringconfig = file_get_contents(ROOT . D_S . "src" . D_S . "app" . D_S . "config" . D_S . "database.json");
+        $json_config = json_decode($stringconfig, true);
+
+
+        $this->DB = $json_config['$DB'];
+        $this->dbhost = $json_config['$dbhost'];
+        $this->dbuser = $json_config['$dbuser'];
+        $this->dbpass = $json_config['$dbpass'];
+        $this->dbname = $json_config['$dbname'];
     }
-  
-    
-    
+
 }
