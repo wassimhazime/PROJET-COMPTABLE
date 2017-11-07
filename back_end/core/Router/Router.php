4541bc1cdf2fc3f7392ee1af461d7d794d1adbdd
wassimhazime <wassim.hazime@gmail.com>
2017-11-07 12:45:00 +0100

    ok

--- back_end/core/Router/Router.php
+++ back_end/core/Router/Router.php
@@ -1,69 +1,62 @@
 <?php
 
 namespace core\Router;
-use function Http\Response\send;
-use Psr\Http\Message\ResponseInterface ;
+
+use Psr\Http\Message\ResponseInterface;
 use GuzzleHttp\Psr7\Response;
 
-
 class Router {
-    private $siApatch='';
+
+    private $siApatch = '';
+
     function setSiApatch($siApatch) {
         $this->siApatch = $siApatch;
     }
 
-        
     private $routes = [];
     private $namedroutes = [];
 
-   
-
-    public function get(string $path, callable $callable, string $name=""): Route {
-        return $this->method("GET", $path, $callable,$name);
+    public function get(string $path, callable $callable, string $name = ""): Route {
+        return $this->method("GET", $path, $callable, $name);
     }
 
-    public function post(string $path, callable $callable, string $name=""): Route {
-        return $this->method("POST", $path, $callable,$name);
+    public function post(string $path, callable $callable, string $name = ""): Route {
+        return $this->method("POST", $path, $callable, $name);
     }
 
     private function method(string $method, string $path, callable $callable, string $name) {
-        $route = new Route($path, $callable,$name,$this->siApatch);
-        
+        $route = new Route($path, $callable, $name, $this->siApatch);
+
         $this->routes[$method][] = $route;
-        $this->namedroutes[$route->getNameRoute()]=$route;
+        $this->namedroutes[$route->getNameRoute()] = $route;
         return $route;
     }
 
-    public function run($Request):ResponseInterface {
+    public function run($Request): ResponseInterface {
         $Response = new Response();
-        
+
         foreach ($this->routes[$Request->getMethod()] as $route) {
-            
-            
             if ($route->match($Request)) {
-             return   $route->call($Request, $Response);
-                 break; 
-                
+                return $route->call($Request, $Response);
+                break;
             }
-         }
-        
-        return new Response(404);
-    }
-    
-    ///tools
-    public function get_URL(string $name, array $param=[]) {
-        if(!isset($this->namedroutes[$name])){
-           throw new \Exception(" not is name route"); 
         }
-        $route=$this->namedroutes[$name];
-        return  $route->url($param);
-       
+
+        return ((new Response(404))->withHeader('Location', "/"));
     }
-    public function redirection(string $name, array $param=[]) {
-        $url= $this->get_URL($name, $param);
+
+    ///tools
+    public function get_URL(string $name, array $param = []) {
+        if (!isset($this->namedroutes[$name])) {
+            throw new \Exception(" not is name route");
+        }
+        $route = $this->namedroutes[$name];
+        return $route->url($param);
+    }
+
+    public function redirection(string $name, array $param = []) {
+        $url = $this->get_URL($name, $param);
         return ((new Response(301))->withHeader('Location', $url));
-        
-       
     }
 
 }
